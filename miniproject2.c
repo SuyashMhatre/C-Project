474d32ab80accacb97fb4a407fefdaa8e43e2c7a
SuyashMhatre <suyashmhatre270@gmail.comm>
2024-07-10 20:10:32 +0530

    first commit

---
+++ miniproject2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Movie {
+    char name[100];
+    int duration;
+    int category; 
+    int ticketCost;
+};
+
+struct Movie GetMovieDetails() {
+    struct Movie movie;
+    printf("\nWelcome To INOX (Pune)\n");
+    printf("\nLIVE THE MOVIE\n");
+    printf("\nToday's Movies Shows :(Marval Festival)(Screen : 01) :-\n");
+    printf("\n1. Iron Man : (Time : 8.00 AM)\n");
+    printf("2. Captain America : (Time : 12.00 PM)\n");
+    printf("3. DoctarStrange:Multivers of madness : (Time : 3.00 PM)\n");
+    printf("4. SpiderMan:No Way Home : (Time : 6.00 PM)\n");
+    printf("5. Avengers:End Game :  (Time : 10.00 PM)\n");
+    printf("\nEnter movie name: ");
+    scanf("%s", &movie.name);
+    printf("Enter movie duration (in hours): ");
+    scanf("%d", &movie.duration);
+    printf("Enter movie category (2 for 2D, 3 for 3D): ");
+    scanf("%d", &movie.category);
+    return movie;
+}
+
+/**char* GetCircleDetails() {
+    static char circle[10];
+    printf("Enter circle (silver/gold/platinum): ");
+    scanf("%s", circle);
+    return circle;*/
+}
+
+int CalculateTicketCost(char* circle, int category) {
+    if (strcmp(circle, "gold") == 0) {
+        if (category == 2)
+            return 300;
+        else if (category == 3)
+            return 500;
+    } else if (strcmp(circle, "silver") == 0) {
+        if (category == 2)
+            return 250;
+        else if (category == 3)
+            return 450;
+    } else if (strcmp(circle, "platinum") == 0) {
+        if (category == 2)
+            return 520;
+        else if (category == 3)
+            return 620;
+    }
+    return -1;
+}
+ 
+
+
+int main() {
+    struct Movie movie = GetMovieDetails();
+    char* circle = GetCircleDetails();
+    int ticketCost = CalculateTicketCost(circle, movie.category);
+
+    if (ticketCost == -1) {
+        printf("Sorry there is no %s type of category in the theatre.\n", movie.category == 2 ? "2D" : "3D");
+    } else if (ticketCost == -2) {
+        printf("Sorry the circle is invalid.\n");
+    } else if (ticketCost == -3) {
+        printf("Sorry both circle and category are invalid.\n");
+    } else {
+
+         printf("\n---------------------------------");
+        printf("\n Your Movie Details :-\n");
+        printf(" Movie Name : %s\n", movie.name);
+        printf(" Movie Duration in hours : %d\n", movie.duration);
+        printf(" Movie Category(2 for 2D, 3 for 3D) : %d\n", movie.category);
+        printf("Final ticket cost is %d\n", ticketCost);
+    }
+
+     return 0;
+}
